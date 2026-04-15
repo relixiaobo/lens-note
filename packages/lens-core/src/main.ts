4b45ebb0d75dd2e6409739a55a00ceca67a07c69
relixiaobo <lixiaobock@gmail.com>
2026-04-15 17:56:12 +0800

    Bump to v1.12.1 for retype reason fix publish

--- packages/lens-core/src/main.ts
+++ packages/lens-core/src/main.ts
@@ -147,7 +147,7 @@ Errors:  {"ok": false, "error": {"code": "...", "message": "..."}}
 }
 
 if (command === "--version" || command === "-v") {
-  console.log("lens v1.12.0");
+  console.log("lens v1.12.1");
   process.exit(0);
 }
 
